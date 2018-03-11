0b7de7ad566667fa943acf9b6fc7564638fb1923
chenxiptz <chenxiptz@gmail.com>
2018-03-10 19:21:13 -0600

    fn headers

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -5,22 +5,45 @@
 #include "rtc.h"
 #include "lib.h"
 
-/* Initialize the rtc */
+/* 
+ * rtc_init 
+ * DESCRIPTION: initialize the rtc
+ * INPUTS: none
+ * OUTPUTS: none
+ * RETURN VALUE: none
+ * SIDE EFFECTS: writes to rtc control register, enables irq8 on PIC
+ */
 void rtc_init(){
-                                            // disable interrupts
-    outb(REG_B_NMI, RTC_PORT);		            // select register B, and disable NMI
-    unsigned char prev = inb(CMOS_PORT);    // read the current value of register B
-    outb(REG_B_NMI, RTC_PORT);	                // set the index again
-    outb(prev | BIT_SIX, CMOS_PORT);	    // turns on bit 6 of register B
     
+    /* selects register B */
+    outb(REG_B_NMI, RTC_PORT);
+    /* read and store current value */
+    unsigned char prev = inb(CMOS_PORT);
+    /* set the register to register B again */
+    outb(REG_B_NMI, RTC_PORT);
+    /* turns on bit 6 of register B */
+    outb(prev | BIT_SIX, CMOS_PORT);
+    /* enable the corresponding irq line on PIC */
     enable_irq(RTC_IRQ_NUM);
 
 }
 
-/* interrupt handler(for now) */
+/* 
+ * rtc_handler
+ * DESCRIPTION: temporary rtc interrupt handler, 
+ * only calls test_interrupte() to make sure that
+ * we are able to receive interrupt
+ * INPUTS: none
+ * OUTPUTS: none
+ * RETURN VALUE: none
+ * SIDE EFFECTS: changes video memory content
+ */
 void rtc_handler(){
+    /* sends eoi */
     send_eoi(RTC_IRQ_NUM);
+    /* calls tet function in lib.c */
     test_interrupts();
-    outb(REG_C, RTC_PORT);	// select register C
+    /* reads from register C so that the interrupt will happen again */
+    outb(REG_C, RTC_PORT);
     inb(CMOS_PORT);
 }
