c61d7c46daad08af59e02c1d3ab9320528e85076
Yiyi Wang <>
2018-03-25 15:46:25 -0500

    add documentation

--- student-distrib/rtc.h
+++ student-distrib/rtc.h
@@ -17,8 +17,17 @@
 
 /* rtc ports */
 #define RTC_PORT    0x70
+/**
+ *	RTC_PORT
+ *
+ *	Port for writing to RTC
+ */
 #define CMOS_PORT   0x71
-
+/**
+ *	CMOS_PORT
+ *
+ *	Port for reading from RTC
+ */
 #define REG_A       0x0A
 #define REG_B       0x0B
 #define REG_C       0x0C
@@ -46,11 +55,17 @@ void rtc_init();
 /**
  *	Temporary rtc interrupt handler
  *
- *	Currently only calls test_interrupte() to make sure that we are able to
+ *	Currently only calls test_interrupts() to make sure that we are able to
  *	receive interrupt. Changes video memory content
  */
 void rtc_handler();
 
+/**
+ *	Temporary rtc interrupt handler
+ *
+ *	Currently only called by rtc_test_2 to check rtc functionality.
+ *
+ */
 void test_rtc_handler();
 
 /**
@@ -61,18 +76,43 @@ void test_rtc_handler();
 void rtc_setrate(int rate);
 
 /**
- *	Initialize frequency to 2 Hz and enable RTC
+ *	Initialize frequency to 2 Hz and enable RTC by changing rtc_status. 
  *
- *	Occured when RTC needs to be turned on and initialized.
+ *	Occured when RTC needs to be turned on.
  */
 
 int rtc_open();
 
+/**
+ *	Close opened rtc and free all the private data.
+ *	
+ *	Currently do nothing.
+ */
+
 int rtc_close();
+
+/**
+ *	Block rtc interrupts at a given frequency. (virtualized)
+ *
+ *	While reading, process will not do anyother thing until count reach 
+ *	a giving frequency.
+ */
 
 int rtc_read();
 
+/**
+ *	Set frequency of RTC interrupts (virtualized)
+ *
+ *	@param freq: The RTC interrupt frequency (exact frequency). Must be in the range between 2-1024Hz and must be power of 2.
+ */
+
 int rtc_write(int freq);
+
+/**
+ *	Check if a number is a power of 2.
+ *
+ *	@param freq: An input number to be checked.
+ */
 
 int is_power_of_two(int freq);
 
