e63613182b89db37745faa614a9fd61e1c2787cb
Yiyi Wang <>
2018-04-20 23:44:50 -0500

    fix kmalloc implement

---
+++ student-distrib/k_mem/kmalloc.c
@@ -0,0 +1,167 @@
+#include "kmalloc.h"
+
+static struct memory_list mem_info[SLAB_NUM];
+static struct memory_list *free_list;
+static struct memory_list *alloc_list;
+
+static char status = 0;								// indicate if memory pool is initialized
+static int 	start_addr;
+static int 	cur_addr = 0;							// indicate virtual addr for next allocated starting page addr
+
+void kmalloc_init() {
+
+	int i; 			// iterator
+	int addr = 0;	// request addr from page;
+
+	for (i = 2; i < SLAB_NUM; i++) {
+		mem_info[i].status = 0;
+		mem_info[i].size = 0;
+		mem_info[i].prev = NULL;
+		mem_info[i].info = NULL;
+		mem_info[i].next = NULL;
+	}
+
+	// REQUESTE 3 4MB PAGES
+	page_alloc_4MB(&addr);
+	start_addr = addr;
+	cur_addr = addr + (4 * MEGA_BYTE);
+	page_dir_add_4MB_entry(addr, addr, PAGE_DIR_ENT_PRESENT | PAGE_DIR_ENT_RDWR |
+						PAGE_DIR_ENT_SUPERVISOR | PAGE_DIR_ENT_4MB |
+						PAGE_DIR_ENT_GLOBAL);
+
+	status = 1;
+	get_free_page();
+	get_free_page();
+/*
+	memory_pool[0].status = 0;
+	memory_pool[0].start_addr = start_addr;
+	memory_pool[0].size = SLAB_NUM;
+*/
+	((malloc_info_t*)start_addr)->start_addr = start_addr;
+	((malloc_info_t*)start_addr)->size = SLAB_NUM;
+
+	free_list = &(mem_info[0]);
+	free_list->status = 1;
+	free_list->size = SLAB_NUM;
+	free_list->prev = NULL;
+	free_list->info = ((malloc_info_t*)start_addr);
+	free_list->next = NULL;
+
+	alloc_list = &(mem_info[1]);
+	alloc_list->status = 1;
+	alloc_list->size = 0;
+	alloc_list->prev = NULL;
+	alloc_list->info = NULL;
+	alloc_list->next = NULL;
+
+	status = 1;
+}
+
+int get_free_page() {
+
+	if (status == 0) {
+		return -ENOBUFS;
+	}
+
+	if (cur_addr - start_addr >= (12 * MEGA_BYTE)) {
+		return -ENOBUFS;
+	}
+
+	int addr = 0;
+	page_alloc_4MB(&addr);
+	page_dir_add_4MB_entry(cur_addr, addr, PAGE_DIR_ENT_PRESENT | PAGE_DIR_ENT_RDWR |
+					PAGE_DIR_ENT_SUPERVISOR | PAGE_DIR_ENT_4MB |
+					PAGE_DIR_ENT_GLOBAL);
+	cur_addr += (4 * MEGA_BYTE);
+
+	return 0;
+}
+
+int ceiling_division(int x, int y) {
+	if ((x % y) != 0)
+		return (x/y) + 1;
+	else
+		return x/y;
+}
+void* kmalloc(size_t size) {
+
+	if (status == 0) {
+		kmalloc_init();
+	}
+
+	int num_slab;
+	int addr;
+	int i;
+
+	memory_list_t *temp, *temp2;
+	temp = free_list;
+
+	num_slab = ceiling_division((size + INFO_SIZE),SLAB_SIZE);
+
+	while(temp != NULL) {
+		if (temp->size >= num_slab)
+			break;
+		temp = temp->next;
+	}
+
+	if (temp->size - num_slab == 0) {
+		
+		addr = temp->info->start_addr;
+
+		if (temp->prev != NULL)
+			temp->prev->next = temp->next;
+		if (temp->next != NULL)
+			temp->next->prev = temp->prev;
+
+		temp2 = alloc_list;
+
+		while (temp2->next != NULL) {
+			temp2 = temp2->next;
+		}
+
+		temp2->next = temp;
+		temp->prev = temp2;
+		temp->next = NULL;
+
+	} else {
+		addr = temp->info->start_addr;
+		// temp for allocated memory, find new place for free slabs
+		malloc_info_t *new_info;
+		new_info = (malloc_info_t*)(addr + (num_slab*SLAB_SIZE));
+		for (i = 2; i < SLAB_NUM; i++) {
+			if (mem_info[i].status == 0) {
+				temp2 = &(mem_info[i]);
+				temp2->status = 1;
+				break;
+			}
+		}
+		temp2->size = temp->size - num_slab;
+		temp2->prev = temp->prev;
+		temp2->info = new_info;
+		temp2->info->start_addr = temp->info->start_addr + num_slab*SLAB_SIZE;
+		temp2->info->size = temp2->size;
+		temp2->next = temp->next;
+
+		if (temp->prev != NULL)
+			temp->prev->next = temp2;
+		if (temp->next != NULL)
+			temp->next->prev = temp2;
+
+		temp->size = num_slab;
+		temp->info->start_addr = addr;
+		temp->info->size = num_slab;
+
+		temp2 = alloc_list;
+
+		while (temp2->next != NULL) {
+			temp2 = temp2->next;
+		}
+
+		temp2->next = temp;
+		temp->prev = temp2;
+		temp->next = NULL;
+	}
+
+	return (void*)(addr+INFO_SIZE);
+}
+
---
+++ student-distrib/k_mem/kmalloc.h
@@ -0,0 +1,39 @@
+/**
+ *	@file kmalloc.h
+ *
+ *	kmalloc implementation and memory management
+ */
+
+#ifndef KMALLOC_H_
+#define KMALLOC_H_
+
+#include "../boot/page_table.h"
+
+#define MEGA_BYTE 	0x00100000
+#define SLAB_SIZE 	0x00000200 				///< SLAB_SIZE = 512 bytes
+#define KMEM_POOL 	0x00C00000				///< memory pool size = 12MB
+#define SLAB_NUM	(KMEM_POOL/SLAB_SIZE)	///< total number of slabs in memory pool = 8192
+#define INFO_SIZE 	sizeof(malloc_info_t)
+
+typedef struct malloc_info{
+	int start_addr;	///< the starting address of space
+	int size;		///< number of slabs;
+} malloc_info_t;
+
+typedef struct memory_list {
+	char status;	///< 0 for not used;
+	int size;
+	struct memory_list *prev;		///< addr to the previous linked memory
+	struct malloc_info *info;		///< malloc info
+	struct memory_list *next;		///< pointer to the next item, NULL for last item
+} memory_list_t;
+
+void kmalloc_init();
+
+int get_free_page();
+
+int ceiling_division(int x, int y);
+
+void* kmalloc(size_t size);
+
+#endif
